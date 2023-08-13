eba3f927c22fdf36c38630c86212d1f2eb251552
iris-gk <91878524+iris-gk@users.noreply.github.com>
2023-08-13 18:51:25 -0400

    initial module changes

---
+++ src/app/submission/search-submission/search-submission.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-submission',
+  templateUrl: './search-submission.component.html',
+  styleUrls: ['./search-submission.component.css']
+})
+export class SearchSubmissionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
